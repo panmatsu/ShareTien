0b9207a0df1c787ba4cb57e458515085192e4556
matsu <5bji1214@mail.u-tokai.ac.jp>
2016-06-06 00:55:48 +0900

    TweetActivityでのGPS失敗

--- app/src/main/java/org/t_robop/matsu/sharetien/MainActivity.java
+++ app/src/main/java/org/t_robop/matsu/sharetien/MainActivity.java
@@ -1,17 +1,7 @@
 package org.t_robop.matsu.sharetien;
 
-import android.Manifest;
-import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.location.Address;
-import android.location.Criteria;
-import android.location.Geocoder;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.os.Bundle;
-import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -25,22 +15,17 @@ import com.twitter.sdk.android.core.TwitterAuthConfig;
 import com.twitter.sdk.android.core.TwitterException;
 import com.twitter.sdk.android.core.TwitterSession;
 import com.twitter.sdk.android.core.identity.TwitterLoginButton;
-import com.twitter.sdk.android.core.internal.TwitterCollection;
-
-import java.io.IOException;
-import java.util.List;
-import java.util.Locale;
 
 import io.fabric.sdk.android.Fabric;
 
-public class MainActivity extends AppCompatActivity implements LocationListener{
+public class MainActivity extends AppCompatActivity {
 
     // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
     private static final String TWITTER_KEY = "Nqt9lJurtSTsU1MD36LrzyD06";
     private static final String TWITTER_SECRET = "GSbJVYO5PbyaLa763jZnkLoX2dtTvxkkhkb38MhPk5dbKkDQkc";
 
     //GPS用
-    private LocationManager mLocationManager;
+    //private LocationManager mLocationManager;
 
 
     //ログインボタンの宣言
@@ -56,34 +41,6 @@ public class MainActivity extends AppCompatActivity implements LocationListener{
         Fabric.with(this, new Twitter(authConfig));
         setContentView(R.layout.activity_main);
 
-        //GPS
-        mLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        boolean gpsFlg = mLocationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
-        Log.d("GPS Enabled", gpsFlg ? "OK" : "NG");
-
-        // Criteriaオブジェクトを生成
-        Criteria criteria = new Criteria();
-
-        // Accuracyを指定(低精度)
-        criteria.setAccuracy(Criteria.ACCURACY_COARSE);
-
-        // PowerRequirementを指定(低消費電力)
-        criteria.setPowerRequirement(Criteria.POWER_LOW);
-
-        // ロケーションプロバイダの取得
-        String provider = mLocationManager.getBestProvider(criteria, true);
-
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-        mLocationManager.requestLocationUpdates(provider, 0, 0, this);
         //ログインボタンの実装
         //Twitterのボタンの関連付け
         loginButton = (TwitterLoginButton) findViewById(R.id.twitter_login_button);
@@ -111,100 +68,6 @@ public class MainActivity extends AppCompatActivity implements LocationListener{
                 Log.d("TwitterKit", "Login with Twitter failure", exception);
             }
         });
-
-    }
-
-
-    // GPSボタン
-    public void onBtnGpsClicked(View view) {
-
-        mLocationManager.requestLocationUpdates(
-                LocationManager.GPS_PROVIDER, //LocationManager.NETWORK_PROVIDER,
-                3000, // 通知のための最小時間間隔（ミリ秒）
-                10, // 通知のための最小距離間隔（メートル）
-                new LocationListener() {
-                    @Override
-                    public void onLocationChanged(Location location) {
-                        String msg = "Lat=" + location.getLatitude()
-                                + "\nLng=" + location.getLongitude();
-                        Log.d("GPS", msg);
-                        if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                            // TODO: Consider calling
-                            //    ActivityCompat#requestPermissions
-                            // here to request the missing permissions, and then overriding
-                            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                            //                                          int[] grantResults)
-                            // to handle the case where the user grants the permission. See the documentation
-                            // for ActivityCompat#requestPermissions for more details.
-                            return;
-                        }
-                        mLocationManager.removeUpdates(this);
-
-                    }
-
-                    @Override
-                    public void onProviderDisabled(String provider) {
-                    }
-
-                    @Override
-                    public void onProviderEnabled(String provider) {
-                    }
-
-                    @Override
-                    public void onStatusChanged(String provider, int status, Bundle extras) {
-                    }
-                }
-        );
-
-
-
-//        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-//            // TODO: Consider calling
-//            //    ActivityCompat#requestPermissions
-//            // here to request the missing permissions, and then overriding
-//            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-//            //                                          int[] grantResults)
-//            // to handle the case where the user grants the permission. See the documentation
-//            // for ActivityCompat#requestPermissions for more details.
-//            return;
-//        }
-//        mLocationManager.requestLocationUpdates(
-//                LocationManager.GPS_PROVIDER, //LocationManager.NETWORK_PROVIDER,
-//                3000, // 通知のための最小時間間隔（ミリ秒）
-//                10, // 通知のための最小距離間隔（メートル）
-//                new LocationListener() {
-//                    @Override
-//                    public void onLocationChanged(Location location) {
-//                        String msg = "Lat=" + location.getLatitude()
-//                                + "\nLng=" + location.getLongitude();
-//                        Log.d("GPS", msg);
-//                        if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-//                            // TODO: Consider calling
-//                            //    ActivityCompat#requestPermissions
-//                            // here to request the missing permissions, and then overriding
-//                            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-//                            //                                          int[] grantResults)
-//                            // to handle the case where the user grants the permission. See the documentation
-//                            // for ActivityCompat#requestPermissions for more details.
-//                            return;
-//                        }
-//                        mLocationManager.removeUpdates(this);
-//
-//                    }
-//
-//                    @Override
-//                    public void onProviderDisabled(String provider) {
-//                    }
-//
-//                    @Override
-//                    public void onProviderEnabled(String provider) {
-//                    }
-//
-//                    @Override
-//                    public void onStatusChanged(String provider, int status, Bundle extras) {
-//                    }
-//                }
-//        );
     }
 
 
@@ -241,23 +104,4 @@ public class MainActivity extends AppCompatActivity implements LocationListener{
         startActivity(intent);
     }
 
-    @Override
-    public void onLocationChanged(Location location) {
-
-    }
-
-    @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
-
-    }
-
-    @Override
-    public void onProviderEnabled(String provider) {
-
-    }
-
-    @Override
-    public void onProviderDisabled(String provider) {
-
-    }
 }
--- app/src/main/java/org/t_robop/matsu/sharetien/TweetActivity.java
+++ app/src/main/java/org/t_robop/matsu/sharetien/TweetActivity.java
@@ -1,11 +1,17 @@
 package org.t_robop.matsu.sharetien;
 
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.vision.barcode.Barcode;
 import com.twitter.sdk.android.core.Callback;
 import com.twitter.sdk.android.core.Result;
 import com.twitter.sdk.android.core.TwitterCore;
@@ -20,12 +26,40 @@ public class TweetActivity extends AppCompatActivity {
     //Tweetを入れる用
     String TweetStr;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tweet);
         //Tweetを入力するeditTextの関連付け
         TwitterText = (EditText)findViewById(R.id.editText);
+    }
+
+    //現在地取得ボタンを押した時の処理
+    public void onClick(View view){
+        //LocationManagerの取得
+        LocationManager locationManager = (LocationManager)this.getSystemService(Context.LOCATION_SERVICE);
+        //GPSから現在地の情報を取得
+        Location myLocate = locationManager.getLastKnownLocation("gps");
+        Log.d("TEST",String.valueOf(myLocate));
+        //MapControllerの取得
+        //GoogleMap MapCtrl = mapView.getController();
+        if(myLocate != null){
+            //現在地情報取得成功
+            //緯度の取得
+            int latitude = (int) (myLocate.getLatitude() * 1e6);
+            //経度の取得
+            int longitude = (int) (myLocate.getLongitude() * 1e6);
+            //GeoPointに緯度・経度を指定
+            //Barcode.GeoPoint GP = new Barcode.GeoPoint(latitude, longitude);
+            //現在地までアニメーションで移動
+            //MapCtrl.animateTo(GP);
+            //現在地までパッと移動
+            //MapCtrl.setCenter(GP);
+        }else{
+            //現在地情報取得失敗時の処理
+            Toast.makeText(this, "現在地取得できませーん！", Toast.LENGTH_SHORT).show();
+        }
     }
 
     //ツイートボタンをおした時の処理
@@ -49,14 +83,15 @@ public class TweetActivity extends AppCompatActivity {
         statusesService.update(TweetStr,
                                 /*Long リプライ先ID*/null,
                                 /*Boolean ?*/null,
-                                /*Double 緯度*/a,
-                                /*Double 経度*/i,
+                                /*Double 緯度*/null,
+                                /*Double 経度*/null,
                                                 null, null, null, new Callback<Tweet>() {
 
             //成功した場合
             @Override
             public void success(Result<Tweet> tweetResult) {
-                Log.d("ツイート完了","");
+                Toast.makeText(getApplicationContext(), "ついっとしました", Toast.LENGTH_SHORT).show();
+                TwitterText.setText(null);
             }
 
             //失敗した場合
